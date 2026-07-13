33982304436c63a55043fa611bd9fb0c2df9d998
GK <gopinath18k@gmail.com>
2026-07-13 18:09:52 +0530

    MARS HERO TRUCK ANIMATION UPDATED 13-07-26

--- src/components/ContactLayout/ContactInfo.jsx
+++ src/components/ContactLayout/ContactInfo.jsx
@@ -22,7 +22,7 @@ const ContactInfo = () => {
       title: "TECHNICAL",
       subtitle: "SALES",
       iconImg: contacticon2,
-      details: <>+91 9092 67 93 93 <br /> Mon — Fri, 10:00 - 18:00 IST</>,
+      details: <>+91 9092 67 93 93 <br /> Mon — Sat, 10:00 - 18:00 IST</>,
       linkText: "DIRECT CALL",
       linkSymbol: "📞",
       
